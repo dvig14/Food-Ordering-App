fa144ac31c1c277fc1427404ccbe50c863f59484
Diksha <97282430+dvig14@users.noreply.github.com>
2024-04-25 16:42:29 +0530

    Update ItemsList.js

--- frontend/src/components/ItemsList.js
+++ frontend/src/components/ItemsList.js
@@ -10,7 +10,7 @@ const ItemsList = ({items}) => {
   const {isCart} = useContext(LocationContext)
 
   return(
-    <div className={`${isCart ? 'w-[100%] bg-white' : 'w-full mb-[1rem]'}`}>
+    <div className={`${isCart ? 'w-[100%] bg-white' : 'w-full'}`}>
       {
         items.map((item)=>(
            
