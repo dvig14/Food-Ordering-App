f08932a1a76f880351a4b94eca815cafc6d4f7b6
Diksha <97282430+dvig14@users.noreply.github.com>
2024-05-08 12:43:26 +0530

    Update otpGeneration.js

--- backend/src/utils/otpGeneration.js
+++ backend/src/utils/otpGeneration.js
@@ -1,18 +1,11 @@
 const {Otp} = require('../db/authDb')
 const nodemailer = require('nodemailer');
-/*const twilio = require('twilio')
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID
-const authToken = process.env.TWILIO_AUTH_TOKEN
-
-const twilioClient = new twilio(accountSid,authToken)  */ 
 
 const otpGeneration = async(res,email) => {
-
+   try { 
     const otp = Math.floor(100000 + Math.random() * 900000)
-   // const newPhnNum = `+91${phoneNumber}`
-    
     const emailExist = await Otp.findOne({email})
+    
     if(emailExist){
         const id = emailExist._id
         await Otp.findByIdAndUpdate(id,{otp},{new:true})
@@ -29,34 +22,48 @@ const otpGeneration = async(res,email) => {
          msg:'otp generated'
        })
     }
-     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+    const transporter = nodemailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
         auth: {
           user: process.env.MY_EMAIL,
           pass: process.env.EMAIL_PASS
-         }
+         },
+        secure: true,
     })
-     const mailOptions = {
+    await new Promise((resolve, reject) => {
+       // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+             } else {
+                 console.log("OTP has been sent to your Email");
+                 resolve(success);
+             }
+       });
+    });
+    const message = await transporter.sendMail({
         from: process.env.MY_EMAIL,
         to: email,
         subject: 'Otp from yummy.com',
         text:`This is your Otp:${otp}. Don't share with anyone.`
-      }  
-    try { 
-        console.log('sending...')
-        const info = await transporter.sendMail(mailOptions); 
-         console.log('email sent',info.response)
-        res.json({ msg: `Email sent: ${info.response}` });
+     })
+      await new Promise((resolve, reject) => {
+         transporter.sendMail(message, (err, info) => {
+             if (err) {
+                console.error(err);
+                reject(err);
+              } else {
+                 resolve(info);
+              }
+        });
+     });
     } 
     catch (error) { 
         console.error('Error sending email:', error); 
         res.json({ msg: 'Error sending email' });
     }
-   /*await twilioClient.messages.create({
-        body : `Your OTP is: ${otp} from Yummy`,
-        to : newPhnNum,
-        from : process.env.TWILIO_PHONE_NUMBER
-    })*/
 }
 
 module.exports = otpGeneration
